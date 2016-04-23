9c1892e9fc949da29f77e8cd4e591e1a19900bc6
weiliangc3 <weiliang.c3@gmail.com>
2016-04-23 10:41:07 +0100

    routes and seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,10 @@
 Rails.application.routes.draw do
 
 
-  get 'wishes/new'
-  get 'wishes/create'
-  get 'wishes/edit'
-  get 'wishes/update'
-  get 'wishes/delete'
-
-  get 'attendances/new'
-  get 'attendances/index'
-  get 'attendances/create'
-  get 'attendances/delete'
-
-  resources :weddings
+  resources :weddings do
+    resources :wishes, except: [:index, :show]
+    resources :attendances, except: [:edit, :show]
+  end
   devise_for :users
   root 'statics#home'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+Wedding.destroy_all
+Attendance.destroy_all
+Wish.destroy_all
+
+u1 = User.new(email: "wei@wei.com",
+              password: "password",
+              first_name: "Wei",
+              last_name: "Cheah")
+
+u2 = User.new(email: "john@john.com",
+              password: "password",
+              first_name: "John",
+              last_name: "Segar")
+
+u3 = User.new(email: "cat@cat.com",
+              password: "password",
+              first_name: "Cat",
+              last_name: "Lau")
+
+w1 = Wedding.new( name: "WeiDing",
+                  description: "Wei's Test Wedding")
+
+a1 = Attendance.new(role: "couple",
+                    status: "confirmed",
+                    email: "wei@wei.com",
+                    user_id: u1.id,
+                    wedding_id: w1.id)
+
+a2 = Attendance.new(role: "admin",
+                    status: "confirmed",
+                    email: "cat@cat.com",
+                    user_id: u3.id,
+                    wedding_id: w1.id)
+
+a3 = Attendance.new(role: "guest",
+                    status: "pending",
+                    email: "john@john.com",
+                    wedding_id: w1.id)
+
+wi1 = Wish.new( name: "Pan",
+                details: "I really want a pan. Teflon please",
+                wedding_id: w1.id)
+
+wi1 = Wish.new( name: "Gamecube",
+                details: "I really want a Gamecube, Smash Bros please",
+                wedding_id: w1.id)
+
+wi1 = Wish.new( name: "Duvet",
+                details: "I really want a duvet. 50 tog please",
+                wedding_id: w1.id)
+
+wi1 = Wish.new( name: "Guitar",
+                details: "I really want a guitar. Fender acoustic please",
+                wedding_id: w1.id)
