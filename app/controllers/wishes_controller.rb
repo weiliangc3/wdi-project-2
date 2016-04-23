0c9115203d83d2c123c7d39dcbeb05071fae3643
weiliangc3 <weiliang.c3@gmail.com>
2016-04-23 23:52:32 +0100

    made the claim/unclaim modals and routes

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -12,5 +12,26 @@ class WishesController < ApplicationController
   end
 
   def delete
+    wish = Wish.find(params[:id])
+    
   end
+
+  def claim
+    wish = Wish.find(params[:id])
+    wish.user_id = current_user.id
+    wish.save!
+    redirect_to wedding_path(params[:wedding_id])
+  end
+
+  def unclaim
+    wish = Wish.find(params[:id])
+    wish.user_id = nil
+    wish.save!
+    redirect_to wedding_path(params[:wedding_id])
+  end
+
+  private
+    def wish_params
+      params.require(:wish).permit(:name, :image, :details, :user_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
 
 
   resources :weddings do
-    resources :wishes, except: [:index, :show]
+    resources :wishes, except: [:index, :show] do
+      member do
+        patch :claim
+        patch :unclaim
+      end
+    end
     resources :attendances, except: [:edit, :show]
   end
   devise_for :users
