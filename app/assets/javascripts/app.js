0f3e56ec7e97b103ca17761d12b1bcda59c5e1bf
weiliangc3 <weiliang.c3@gmail.com>
2016-04-28 11:14:47 +0100

    making jscript fade

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,7 +6,7 @@ var fader = fader || {}
 fader.init = function(){
   
   for (i = 0; i < $(".parallax-window").length; i++){
-    $(".parallax-window")[i].mouseover(
+    $(".mouseover-show")[i].mouseover(
       console.log("hover found")
       // $(".parallax-mirror")[($(".parallax-window").length - i)].css( "-webkit-filter", "grayscale(0%)")
       // ,
