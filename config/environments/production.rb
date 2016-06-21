b4eeafbeb47ac6e81d1031bc848ce6eea321f0cd
weiliangc3 <weiliang.c3@gmail.com>
2016-06-21 15:58:32 +0100

    mail done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,15 @@
 Rails.application.configure do
+
+  config.action_mailer.default_url_options = { :host => 'your.websitedomain.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
